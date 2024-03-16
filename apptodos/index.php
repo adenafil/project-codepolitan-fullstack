038fa0758c453799cb48486473e7bfe916ca1fff
adenafil <nafilie9@gmail.com>
2024-03-16 13:30:13 +0700

    feat: add checkbox and change status on click
    This commit adds a checkbox to each todo item and changes the status of the item when the checkbox is clicked.
    The status is stored in the todo.txt file.
    
    Additionally, the commit:
    - Refactors the code to use a foreach loop to iterate over the todos.
    - Adds a function to change the status of a todo item.
    - Modifies the HTML to add a checkbox to each todo item and a click event listener to change the status of the item when the checkbox is clicked.

--- apptodos/index.php
+++ apptodos/index.php
@@ -14,12 +14,26 @@ if (isset($_POST['todo'])) {
     $data = $_POST['todo'];
     $todos[] = [
         'todo' => $data,
-        'status'=> 0
+        'status' => 0
     ];
     $daftar_belanja = serialize($todos);
     file_put_contents('todo.txt', $daftar_belanja);
+    // Redirect halaman
+    header('location:index.php');
 }
 
+if (isset($_GET['status'])) {
+    // ubah status
+    // $_GET['key']
+    // key = index && status is index of key
+    $todos[$_GET['key']]['status'] = $_GET['status'];
+    $daftar_belanja = serialize($todos);
+    file_put_contents('todo.txt', $daftar_belanja);
+    // Redirect halaman
+    header('location:index.php');
+}
+
+print_r($_GET);
 ?>
 
 <h1>Todo App</h1>
@@ -30,13 +44,13 @@ if (isset($_POST['todo'])) {
 </form>
 <ul>
 
-    <?php foreach($todos as $key => $value) { ?>
+    <?php foreach ($todos as $key => $value) { ?>
 
-    <li>
-        <input type="checkbox" name="todo">
-        <label"><?= $value['todo'] ?></label>
-        <a href="#">hapus</a>
-    </li>
+        <li>
+            <input type="checkbox" onclick="window.location.href='index.php?status=<?= ($value['status'] == 1) ? '0' : '1' ?>&key=<?php echo $key; ?>'" name="todo" <?= ($value['status'] == 1) ? 'checked' : '' ?>>
+            <label"><?= ($value['status'] == 1) ? "<del>" . $value['todo'] . "</del>" : $value['todo']  ?></label>
+                <a href="#">hapus</a>
+        </li>
 
-    <?php }?>
+    <?php } ?>
 </ul>
